d25fadc90240b4791f0ea4e5c96d78f22d80ea75
kalyanMittinti <kalyanvenkateswarlu50@gmail.com>
2024-03-17 19:50:44 +0530

    first commit

---
+++ Cloth.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navToggle = document.querySelector('.nav-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    navToggle.addEventListener('click', function () {
+        navLinks.classList.toggle('show');
+    });
+});
